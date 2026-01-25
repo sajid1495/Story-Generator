b13ec97daedb6d2fbe69647f767205ef43cc142a
MD.SAJEDUL ISLAM <m.sajid1495@gmail.com>
2026-01-25 22:35:02 +0600

    completed backend structure and build the app for the first time

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 
+
+app = FastAPI(
+    title="Create Your Own Story.",
+    description="An API to create and manage your own stories.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
